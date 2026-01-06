55b4467553b3a5698f49ed84742998a17d56ee1c
Owais Solanki <azulraza786@gmail.com>
2026-01-06 12:18:56 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+//rest object
+const app = express()
+//middlewares
+//routes
+app.get('/test', (req, res) => {
+res.status(200).send('<h1>Welcome to the Backend</h1>')
+})
+//port
+const PORT = 8080
+//listen
+app.listen(PORT, () => {
+console.log("Server Running");
+})
